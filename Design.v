8a02568afc432fdb7ffb3b5bb41cb92466864054
Subha175 <123578848+Subha175@users.noreply.github.com>
2023-07-05 23:37:03 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,1 +1,20 @@
-
+module electronic_lock(input clk,rst,b0,b1,output unlock,reg [2:0]state);
+  reg [2:0]st;
+  parameter SRST = 3'd0 ,S0 = 3'd1 , S01= 3'd2 ,S010 = 3'd3, S0101= 3'd4 , S01011 = 3'd5 ;
+  always @(posedge clk) begin
+    if(rst)
+      st <= SRST;
+  else
+    case(st)
+      SRST : if(b0) st= S0; else if(b1) st = SRST;
+      S0 : if(b0) st= S0; else if(b1) st = S01;
+      S01 : if(b0) st= S010; else if(b1) st = SRST;
+      S010 : if(b0) st= S0; else if(b1) st = S0101;
+      S0101 : if(b0) st= S010; else if(b1) st = S01011;
+      S01011 : if(b0) st= S0; else if(b1) st = SRST;
+      default st = SRST;
+    endcase
+  end
+  assign unlock = (st== S01011) ;
+  assign state = st;
+endmodule
